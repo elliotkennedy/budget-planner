29d6b958816ff727982e4fd08d3883c5fb86e332
Elliot Kennedy <elliot.kennedy@elkendev.org>
2016-06-26 13:57:10 +0100

    Adding angular2 with typescript

--- server/app.js
+++ server/app.js
@@ -7,25 +7,19 @@ var bodyParser = require('body-parser');
 
 var dbManager = require('./modules/db-manager');
 
-var routes = require('./routes/index');
 var users = require('./routes/user');
 var auth = require('./routes/auth');
 var healthcheck = require('./routes/healthcheck');
 
 var app = express();
 
-// view engine setup - using templates to handle errors
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
+app.use(favicon(path.join(__dirname, '../public', 'images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', routes);
 app.use('/user', users);
 app.use('/auth', auth);
 app.use('/health', healthcheck);
@@ -44,7 +38,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -55,7 +49,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,5 @@
 'use strict';
-const User = require('./User');
+const User = require('../models/User');
 
 module.exports = {
 
