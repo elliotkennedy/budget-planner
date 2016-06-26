404eb92208f94eb591b1efb13be612c9e1ec88fb
Elliot Kennedy <elliot.kennedy@elkendev.org>
2016-06-26 23:53:09 +0100

    Adding angular2 with typescript

--- client/components/app.component.ts
+++ client/components/app.component.ts
@@ -5,14 +5,13 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
     directives: [ROUTER_DIRECTIVES],
     selector: 'app',
     template: `<div class="ui container">
-        <div class="center">
-            <img src='https://angular.io/resources/images/logos/standard/shield-large.png'>
-        </div>
-        <router-outlet></router-outlet>
-        <nav class="ui menu inverted teal">
+        <nav class="ui secondary pointing menu">
             <a [routerLink]="['/']" class="item">Home</a>
-            <a [routerLink]="['/contact']" class="item">Contact Me</a>
+            <div class="right menu">
+                <a class="ui item">Logout</a>
+            </div>
         </nav>
+        <router-outlet></router-outlet>
       </div>`
 })
 
--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,10 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './components/app.component';
+import { HTTP_PROVIDERS } from "@angular/http";
 
 import { APP_ROUTER_PROVIDERS } from "./routes";
 
-bootstrap(AppComponent, APP_ROUTER_PROVIDERS);
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS,
+    HTTP_PROVIDERS
+]);
