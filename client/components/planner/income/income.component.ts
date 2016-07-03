0288a14d15d445b83f561fd8b3de4f8eaa58bfe8
Elliot Kennedy <elliot.kennedy@elkendev.org>
2016-07-03 23:56:39 +0100

    Fixing shit

--- client/components/planner/income/income.component.ts
+++ client/components/planner/income/income.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { DoughnutChartDirective } from '../../../directives/doughnutchart.directive';
-import { Budget, BudgetService } from '../../../services/budget.service';
+import { Budget, BudgetService, Expense } from '../../../services/budget.service';
+import {Observable} from "rxjs/Observable";
 
 @Component({
     selector: 'income',
@@ -12,11 +13,43 @@ export class IncomeComponent implements OnInit {
     budget: Budget = new Budget();
     error: String;
 
+    input: any = {};
+
     constructor(private budgetService: BudgetService) {}
 
     ngOnInit() {
 
         this.budgetService.getBudget().subscribe(
+            budget => {
+                this.budget = budget;
+            },
+            error =>  {
+                this.error = <any>error
+            });
+        
+    }
+    
+    addIncome() {
+
+        if (this.incomePopulated()) {
+            var name = this.input.name;
+            var rate = this.input.rate;
+            var amount = this.input.amount;
+            var income = new Expense(name, amount, rate);
+            this.budget.addIncome(income);
+            this.input = {};
+        }
+    }
+    
+    incomePopulated(): Observable<boolean> {
+        var name = this.input.name;
+        var rate = this.input.rate;
+        var amount = this.input.amount;
+        return name && rate && amount;
+    }
+    
+    saveBudget() {
+        this.budgetService.saveBudget(this.budget).subscribe(
             budget => {
                 this.budget = budget;
             },
--- client/services/budget.service.ts
+++ client/services/budget.service.ts
@@ -53,6 +53,12 @@ export class BudgetService {
             .catch(this.handleError);
     }
 
+    saveBudget(budget: Budget): Observable<Budget> {
+        return this.http.post(this.budgetUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
     private extractData(res: Response) {
         let body = res.json();
         return body.data || {};
