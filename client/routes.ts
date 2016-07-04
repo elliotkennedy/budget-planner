b35097bc7c0e2666e6a51df7279ac37634b1057d
Elliot Kennedy <elliot.kennedy@elkendev.org>
2016-07-04 02:48:59 +0100

    Ui stuff

--- client/routes.ts
+++ client/routes.ts
@@ -3,7 +3,9 @@ import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 import {HomeComponent} from "./components/home/home.component";
 import {SignupComponent} from "./components/signup/signup.component";
 import {PlannerComponent} from "./components/planner/planner.component";
-import {LoginRoutes, AUTH_PROVIDERS} from "./components/login/login.routes";
+import {LoginComponent} from "./components/login/login.component";
+import {AuthGuard} from "./components/login/auth.guard";
+import {AuthService} from "./services/auth.service";
 
 const routes: RouterConfig = [
     {
@@ -19,11 +21,15 @@ const routes: RouterConfig = [
         path: 'planner',
         component: PlannerComponent
     },
-    ...LoginRoutes
+    { 
+        path: 'login',
+        component: LoginComponent 
+    }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
     provideRouter(routes),
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    AUTH_PROVIDERS
+    AuthGuard,
+    AuthService
 ];
